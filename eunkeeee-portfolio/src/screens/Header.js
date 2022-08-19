28ea8297ee4da0016ca0be4f4b668d38c9d5a238
eunkeeee <eunkeeeee@gmail.com>
2022-08-19 20:25:32 +0900

    토글 구현 적당히...
    토글을 접은채로 페이지를 키우면 메뉴가 돌아오지를 않음... 미디어 쿼리를 js에 사용해보겟음

--- eunkeeee-portfolio/src/screens/Header.js
+++ eunkeeee-portfolio/src/screens/Header.js
@@ -1,21 +1,35 @@
 import styles from "./Header.module.css";
+import { useState } from "react";
 
 const Header = ({ onClick }) => {
+  const [isOpen, setMenu] = useState(false);
+  const isSmallWindow = useMediaQuery({
+    query: "(max-width:1100px)",
+  });
   return (
     <div>
       <header className={styles.Header} onClick={onClick}>
         <div className={styles.content}>
           <div className={styles.title}>CEK's Portfolio</div>
-          <div className={styles.navigation__menus}>
-            {/* Navigation Bar */}
-            <div className={styles.navigation__menu}>About Me</div>
-            <div className={styles.navigation__menu}>Skills</div>
-            <div className={styles.navigation__menu}>Archiving</div>
-            <div className={styles.navigation__menu}>Projects</div>
-            <div className={styles.navigation__menu}>Career</div>
-          </div>
-          <a href="#" className={styles.navBtn}>
-            <i class="fa-solid fa-bars navBtn"></i>
+          {isOpen ? null : (
+            <div className={styles.navigation__menus}>
+              {/* Navigation Bar */}
+              <div className={styles.navigation__menu}>About Me</div>
+              <div className={styles.navigation__menu}>Skills</div>
+              <div className={styles.navigation__menu}>Archiving</div>
+              <div className={styles.navigation__menu}>Projects</div>
+              <div className={styles.navigation__menu}>Career</div>
+            </div>
+          )}
+          <a
+            href="#"
+            className={styles.toggleBtn}
+            onClick={() => {
+              setMenu((prev) => !prev);
+              console.log(isOpen);
+            }}
+          >
+            <i class="fa-solid fa-bars"></i>
           </a>
         </div>
       </header>
