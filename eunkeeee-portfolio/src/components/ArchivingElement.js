8e5c694731a1bf1400f806e626055a477802afd5
eunkeeee <eunkeeeee@gmail.com>
2022-08-22 11:10:18 +0900

    어렵다,,,

--- eunkeeee-portfolio/src/components/ArchivingElement.js
+++ eunkeeee-portfolio/src/components/ArchivingElement.js
@@ -4,18 +4,20 @@ import styles from "../screens/Archiving.module.css";
 const ArchivingElement = ({ imgSrc, url, p, liArray }) => {
   return (
     <div>
-      <div className={styles.imgWrapper}>
-        <img src={imgSrc} className={styles.elementImg} />
-      </div>
-      <div className={styles.url}>{url}</div>
-      <div className={styles.description}>
-        <p>{p}</p>
-        <ul>
-          {/* {liArray.foreach((element) => {
+      <a className={styles.element} href={url} target="_blank">
+        <div className={styles.imgWrapper}>
+          <img src={imgSrc} className={styles.elementImg} />
+        </div>
+        <div className={styles.url}>{url}</div>
+        <div className={styles.description}>
+          <p>{p}</p>
+          <ul>
+            {/* {liArray.foreach((element) => {
             <li>{element}</li>;
           })} */}
-        </ul>
-      </div>
+          </ul>
+        </div>
+      </a>
     </div>
   );
 };
