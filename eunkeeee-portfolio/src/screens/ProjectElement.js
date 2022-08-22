66e6786a866178b758c41ec92a325fb748f8cc2c
eunkeeee <eunkeeeee@gmail.com>
2022-08-22 17:17:37 +0900

    Project 내용 완성! CSS만 조금 더 만지자

--- eunkeeee-portfolio/src/screens/ProjectElement.js
+++ eunkeeee-portfolio/src/screens/ProjectElement.js
@@ -1,10 +1,79 @@
 import styles from "./ProjectElement.module.css";
 
-const PrjoectElement = ({ imgSrc, title, subtitle, gitHubLink }) => {
+const PrjoectElement = ({
+  imgSrc,
+  title,
+  period,
+  gitHubLink,
+  MainDescription,
+  MainFeature,
+  url,
+  frontend,
+  deployment,
+}) => {
   return (
-    <div className={styles.content}>
-      <img className={styles.img} src={imgSrc} />
-      <h3>{title}</h3>
+    <div className={styles.Project}>
+      <div className={styles.title}>{title}</div>
+      <div className={styles.period}>{period}</div>
+      <div className={styles.info}>
+        <div className={styles.imgs}>
+          {imgSrc}
+          <nav className={styles.nav}>
+            <i class="fa-solid fa-angle-left"></i>
+            4/4
+            <i class="fa-solid fa-angle-right"></i>
+          </nav>
+        </div>
+        <div className={styles.descriptions}>
+          <div className={styles.MainDescription}>{MainDescription}</div>
+          <hr />
+          <ul className="fa-ul">
+            <li className={styles.li}>
+              <span className="fa-li">
+                <i class="fa-solid fa-check"></i>
+              </span>
+              <div>주요 기능</div>
+              <div>{MainFeature}</div>
+            </li>
+            <li className={styles.li}>
+              {" "}
+              <span className="fa-li">
+                <i class="fa-solid fa-check"></i>
+              </span>
+              <div>GitHub</div>
+              <div>
+                <a href={gitHubLink}></a>
+                {gitHubLink}
+              </div>
+            </li>
+            <li className={styles.li}>
+              {" "}
+              <span className="fa-li">
+                <i class="fa-solid fa-check"></i>
+              </span>
+              <div>URL</div>
+              <div>{url}</div>
+            </li>
+            <li className={styles.li}>
+              {" "}
+              <span className="fa-li">
+                <i class="fa-solid fa-check"></i>
+              </span>
+              <div>Frontend</div>
+              <div>{frontend}</div>
+            </li>
+            <li className={styles.li}>
+              {" "}
+              <span className="fa-li">
+                <i class="fa-solid fa-check"></i>
+              </span>
+              <div>Deployment</div>
+              <div>{deployment}</div>
+            </li>
+          </ul>
+        </div>
+      </div>
+      {/* <img className={styles.img} src={imgSrc} />
       <h4>{subtitle}</h4>
       <a target="_blank" href={gitHubLink}>
         GitHub 바로가기
@@ -12,7 +81,7 @@ const PrjoectElement = ({ imgSrc, title, subtitle, gitHubLink }) => {
       <div className={styles.props}>
         <h5>ReactJS</h5>
         <h5>개인</h5>
-      </div>
+      </div> */}
     </div>
   );
 };
